948b8a7884a984a41ea79ff95154a1a334571a77
thiagocaixeta <thiago.caixeta@gmail.com>
2019-11-03 15:04:14 -0300

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Contaador de Pessoas',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            FlatButton(
+              child: Text("+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text("-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+          ],),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      )));
+}
